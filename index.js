d4b29a277719766bdaf964e8231218f74dfdc44d
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-09-03 00:59:14 -0300

    mvp api

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const handler = require('./lib/async-handler')
+const states = require('./lib/states.json')
+const getBuildStatus = require('./lib/get-build-status')
+const getBadgeStream = require('./lib/get-badge-stream')
+const filterExtensionMiddleware = require('./lib/middlewares/filter-extension')
+
+const app = express()
+app.set('query parser', 'simple')
+
+app.get(
+	'/:filename',
+	filterExtensionMiddleware('filename', 'svg'),
+	(req, res, next) => {
+		req.getBuildStatus = () => getBuildStatus({
+			slug: req.params.filename.replace(/\.svg$/, ''),
+			branch: req.query.branch || 'master',
+		})
+		next()
+	},
+	handler(async (req, res) => {
+		const {state = 'unknown'} = await req.getBuildStatus()
+		res.type('svg').set({ETag: state})
+		if(req.get('if-none-match') === state) return res.status(304).end()
+		const badgeStream = await getBadgeStream(states[state] || states.other)
+		badgeStream.pipe(res)
+	})
+)
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
