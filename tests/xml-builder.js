957e921e66be4f3b2dcfd6411a5cc5686a5808ca
Leonardo Dino <leonardo@kunstdesign.com.br>
2018-10-02 22:26:45 -0300

    fix everything

--- tests/xml-builder.js
+++ tests/xml-builder.js
@@ -1,5 +1,10 @@
 const getProjectXML = require('../lib/get-project-xml')
 
+expect.addSnapshotSerializer({
+	test: value => typeof value === 'string' && value.includes('<Projects>'),
+	print: xml => xml,
+})
+
 test('getProjectXML', () => {
 	expect(getProjectXML()).toMatchSnapshot()
 	expect(getProjectXML({key: 'value'})).toMatchSnapshot()
